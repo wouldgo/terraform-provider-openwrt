deaf59446725be5440fe91a9b17e7332030bd745
wouldgo <wouldgo84@gmail.com>
2025-08-02 23:47:24 +0200

    feat(resource): add openwrt_file

--- main.go
+++ main.go
@@ -2,29 +2,19 @@ package main
 
 import (
 	"context"
-	"flag"
 	"log"
 
 	"github.com/foxboron/terraform-provider-openwrt/internal/provider"
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 )
 
-var (
-	version string = "dev"
-)
-
 func main() {
-	var debug bool
-
-	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
-	flag.Parse()
-
 	opts := providerserver.ServeOpts{
-		Address: "registry.terraform.io/foxboron/openwrt",
-		Debug:   debug,
+		Address: "registry.opentofu.org/foxboron/openwrt",
+		Debug:   true,
 	}
 
-	err := providerserver.Serve(context.Background(), provider.New(version), opts)
+	err := providerserver.Serve(context.Background(), provider.New("test"), opts)
 
 	if err != nil {
 		log.Fatal(err.Error())
