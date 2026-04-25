28dafb88a498a70d71f665996c7b4ba55770d3e4
wouldgo <wouldgo84@gmail.com>
2026-04-25 21:18:26 +0200

    chore(documentation): update documentation and examples

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -3,7 +3,15 @@ provider "openwrt" {
   password = "admin"
   remote   = "http://192.168.8.1:8080"
   api_timeouts = {
-    auth = "20s"
+    auth = "20s",
+    opkg = {
+      install_packages = "4m"
+    }
+  }
+  api_attempts = {
+    opkg = {
+      install_packages = 4
+    }
   }
 }
 
